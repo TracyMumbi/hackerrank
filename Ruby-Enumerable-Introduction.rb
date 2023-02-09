52f378f12a369b2ad3ce740e3863e3fad60f4a7e
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2023-02-09 23:18:08 +0300

    Completed Assignment

---
+++ Ruby-Enumerable-Introduction.rb
@@ -0,0 +1,12 @@
+colors = ["Red", "Green", "Blue"]
+
+def iterate_colors(colors)
+    # Your code here
+      array = []
+      colors.each do |c| 
+          array << c
+      end
+      pp array
+      array
+  end
+  iterate_colors(colors)
---
+++ Ruby-Methods-Arguments.rb
@@ -0,0 +1,4 @@
+# Your code here
+def take(a, len)
+    a[len, a.length]
+end
